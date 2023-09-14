23959ca3a178372f98e9dd38c044d9a98ea95508
George Kibe <kiberenge94@gmail.com>
2023-09-14 21:44:51 +0300

    I have setup the environment ready for Selenium Automation

---
+++ Day1/test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+serv_obj = Service("F:\Staff\Softwares\Drivers\geckodriver-v0.33.0-win64\geckodriver.exe")
+
+driver = webdriver.Firefox(service=serv_obj)
+
+driver.get("https://www.facebook.com/")
+
